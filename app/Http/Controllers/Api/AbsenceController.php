1c8655711912d6829d9a23548479cd243da440a0
Elmazroui Kamel <kamel_elmazroui@ens.univ-artois.fr>
2020-05-20 16:09:30 +0200

    Methode des contollers fait pour ABSENCE et CRENEAU

--- app/Http/Controllers/Api/AbsenceController.php
+++ app/Http/Controllers/Api/AbsenceController.php
@@ -5,8 +5,15 @@ namespace App\Http\Controllers\Api;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegistrationFormRequest;
 use App\Http\Resources\AbsenceRessource;
 use App\Model\Absence;
+use http\Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class AbsenceController extends Controller
 {
@@ -14,6 +21,72 @@ class AbsenceController extends Controller
         $absence = Absence::all();
         $data = AbsenceRessource::collection($absence);
         return jsend_success($data);
+    }
+
+    function create(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'motif' => 'required',
+            'justifiee' => 'required',
+            'idCreneau' => 'required',
+            'idEtudiant' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return jsend_fail([
+                "title" => "Creation failed",
+                "body" => $validator->errors()
+            ], 422);
+        }
+        $input = $request->all();
+        $absence = Absence::create($input);
+        $absence->save();
+
+        return jsend_success($absence);
+    }
+
+    function update(Request $request, $id) {
+        try {
+            $absence = Absence::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return jsend_fail([
+                "title" => "Absence not found.",
+            ], 422);
+        }
+
+        $absence->motif = $request->get('motif', $absence->motif);
+        $absence->justifiee = $request->get('justifiee', $absence->justifiee);
+        $absence->save();
+
+        return jsend_success(['absence'=>$absence], 200);
+    }
+
+    public function show($id) {
+        try {
+            $absence = Absence::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return jsend_fail([
+                "title" => "Absence not found.",
+            ], 422);
+        }
+        $data = new AbsenceRessource($absence);
+        return jsend_success( $data);
+    }
+
+    public function destroy($id) {
+        try {
+            $absence = Absence::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return jsend_fail([
+                "title" => "Absence not found.",
+            ], 422);
+        }
+        try {
+            DB::transaction(function () use ($absence) {
+                Storage::disk('public')->delete($absence->document);
+            });
+        } catch (Exception $e) {
+            return jsend_error($e->getMessage(), $e->getCode());
+        }
+        return jsend_success(['message' => 'Absence deleted successfully.'], 204);
     }
 
 }
--- app/Http/Controllers/Api/CreneauController.php
+++ app/Http/Controllers/Api/CreneauController.php
@@ -5,8 +5,84 @@ namespace App\Http\Controllers\Api;
 
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CreneauRessource;
+use App\Model\Creneau;
+use http\Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class CreneauController extends Controller
 {
+    public function index() {
+        $creneau = Creneau::all();
+        $data = CreneauRessource::collection($creneau);
+        return jsend_success($data);
+    }
+
+    function create(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'dateDeDebut' => 'required',
+            'duree' => 'required',
+            'salle' => 'required',
+            'idModule' => 'required',
+            'idGroupe' => 'required',
+            'idEnseignant' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return jsend_fail([
+                "title" => "Creation failed",
+                "body" => $validator->errors()
+            ], 422);
+        }
+        $input = $request->all();
+        $creneau = Creneau::create($input);
+        $creneau->save();
+
+        return jsend_success($creneau);
+    }
+
+    public function destroy($id) {
+        try {
+            $creneau = Creneau::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return jsend_fail([
+                "title" => "Creneau not found.",
+            ], 422);
+        }
+        return jsend_success(['message' => 'Absence deleted successfully.'], 204);
+    }
+
+    public function show($id) {
+        try {
+            $creneau = Creneau::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return jsend_fail([
+                "title" => "Creneau not found.",
+            ], 422);
+        }
+        $data = new CreneauRessource($creneau);
+        return jsend_success( $data);
+    }
+
+    function update(Request $request, $id) {
+        try {
+            $creneau = Creneau::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return jsend_fail([
+                "title" => "Creneau not found.",
+            ], 422);
+        }
+
+        $creneau->dateDeDebut = $request->get('dateDeDebut', $creneau->dateDeDebut);
+        $creneau->duree = $request->get('duree', $creneau->duree);
+        $creneau->salle = $request->get('salle', $creneau->salle);
+        $creneau->idEnseignant = $request->get('idEnseignant', $creneau->idEnseignant);
+        $creneau->save();
+
+        return jsend_success(['creneau'=>$creneau], 200);
+    }
 
 }
